09a3e89dab443ad49fe0638ee10cc49d7ed8904b
optimalisatie <info@optimalisatie.nl>
2018-04-11 13:53:30 +0200

    Github Updater

--- javascript-optimization.php
+++ javascript-optimization.php
@@ -15,6 +15,7 @@ namespace O10n;
  * Version:           0.0.48
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
+ * GitHub Plugin URI: https://github.com/o10n-x/wordpress-javascript-optimization
  * Text Domain:       o10n
  * Domain Path:       /languages
  */
