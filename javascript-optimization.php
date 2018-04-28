026b91511b5a37f02860361d73b2a4391dfc826f
optimalisatie <info@optimalisatie.nl>
2018-04-28 15:58:42 +0200

    Github Updater

--- javascript-optimization.php
+++ javascript-optimization.php
@@ -12,7 +12,7 @@ namespace O10n;
  * @wordpress-plugin
  * Plugin Name:       Javascript Optimization
  * Description:       Advanced Javascript optimization toolkit. Minify, concat/merge, async loading, advanced editor, ES Lint, UglifyJS (professional), beautifier, HTTP/2 Server Push and more.
- * Version:           0.0.51
+ * Version:           0.0.52
  * Author:            Optimization.Team
  * Author URI:        https://optimization.team/
  * GitHub Plugin URI: https://github.com/o10n-x/wordpress-javascript-optimization
@@ -30,7 +30,7 @@ if (defined('WP_INSTALLING') && WP_INSTALLING) {
 }
 
 // settings
-$module_version = '0.0.51';
+$module_version = '0.0.52';
 $minimum_core_version = '0.0.38';
 $plugin_path = dirname(__FILE__);
 
